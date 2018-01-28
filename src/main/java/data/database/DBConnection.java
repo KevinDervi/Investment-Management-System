690d868887b829d1a677ecf83dbb870d19972aaa
KevinDervi <k1502436@kcl.ac.uk>
2018-01-28 18:43:22 +0000

    adding connection pool

--- src/main/java/data/database/DBConnection.java
+++ src/main/java/data/database/DBConnection.java
@@ -1,5 +1,6 @@
 package main.java.data.database;
 
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -50,3 +51,39 @@ public class DBConnection {
 
     }
 }
+
+public class C3P0DataSource {
+    private static C3P0DataSource dataSource;
+    private ComboPooledDataSource comboPooledDataSource;
+
+    private C3P0DataSource() {
+        try {
+            comboPooledDataSource = new ComboPooledDataSource();
+            comboPooledDataSource
+                    .setDriverClass("com.mysql.jdbc.Driver");
+            comboPooledDataSource
+                    .setJdbcUrl("jdbc:mysql://localhost:3306/testdb");
+            comboPooledDataSource.setUser("root");
+            comboPooledDataSource.setPassword("secret");
+        }
+      catch (PropertyVetoException ex1) {
+                ex1.printStackTrace();
+            }
+        }
+
+        public static C3P0DataSource getInstance() {
+            if (dataSource == null)
+                dataSource = new C3P0DataSource();
+            return dataSource;
+        }
+
+        public Connection getConnection() {
+            Connection con = null;
+            try {
+                con = comboPooledDataSource.getConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return con;
+        }
+    }
